1f76adc83b4c05303bc13fcdc4cdc09c26565146
Mesbah Uddin <msbh214@gmail.com>
2024-06-03 23:38:10 +0600

    updated slider, added prettierrc, tailwind sort plugin, and void element removal plugin

--- dist/script.js
+++ dist/script.js
@@ -1,64 +1,88 @@
 // Slider
-(function slider() {
-    const imgSlider = document.querySelector(".carousel__container");
-    const textSlider = document.querySelector(".quotes__container");
+;(function slider() {
+    const imgSlider = document.querySelector('.carousel__container')
+    const textSlider = document.querySelector('.quotes__container')
+    const prevBtn = document.getElementById('prev')
+    const nextBtn = document.getElementById('next')
 
-    const prevBtn = document.getElementById("prev");
-    const nextBtn = document.getElementById("next");
+    const images = Array.from(imgSlider.children)
+    const texts = Array.from(textSlider.children)
 
-    let counter = 1;
-    let animationDuration = 1000;
-    let lastTime = 0;
+    // select first and last ndoes
+    const firstImg = images[0]
+    const lastImg = images[images.length - 1]
+    const firstText = texts[0]
+    const lastText = texts[texts.length - 1]
+
+    let counter = 1
+    let animationDuration = 1000
+    let lastTime = 0
+
+    images.push(cloneImage(firstImg, null, imgSlider))
+    images.unshift(cloneImage(lastImg, firstImg, imgSlider))
+
+    texts.push(cloneImage(firstText, null, textSlider))
+    texts.unshift(cloneImage(lastText, firstText, textSlider))
+
+    // update the container size for added nodes
+    imgSlider.style.width = `${images.length * 100}%`
+    textSlider.style.width = `${images.length * 100}%`
 
     // EventListeners
-    prevBtn.addEventListener("click", prevBtnClickHandler);
-    nextBtn.addEventListener("click", nextBtnClickHandler);
-    imgSlider.addEventListener("transitionend", counterIncrementHandler);
-    nextBtn.focus()
+    prevBtn.addEventListener('click', prevBtnClickHandler)
+    nextBtn.addEventListener('click', nextBtnClickHandler)
+    imgSlider.addEventListener('transitionend', counterIncrementHandler)
 
     // Set the proper slide initially
-    changeSlides();
+    changeSlides()
 
     function nextBtnClickHandler() {
-        moveHandler("right");
+        moveHandler('right')
     }
 
     function prevBtnClickHandler() {
-        moveHandler("left");
+        moveHandler('left')
     }
 
     // Moves the slide container horizontally
     function changeSlides() {
-        imgSlider.style.transform = `translateX(-${counter * 25}%)`;
-        textSlider.style.transform = `translateX(-${counter * 25}%)`;
+        imgSlider.style.transform = `translateX(-${(counter * 100) / images.length}%)`
+        textSlider.style.transform = `translateX(-${(counter * 100) / texts.length}%)`
     }
 
     // Add transitions and update counter
     function moveHandler(direction) {
-        let currentTime = new Date().getTime();
+        let currentTime = new Date().getTime()
         if (currentTime - lastTime < animationDuration) {
-            return;
+            return
         }
-        direction === "right" ? counter++ : counter--;
-        imgSlider.style.transition = "all 0.8s cubic-bezier(0.87, 0, 0.13, 1)";
-        textSlider.style.transition = "all 0.8s cubic-bezier(0.87, 0, 0.13, 1)";
-        changeSlides();
+        direction === 'right' ? counter++ : counter--
+        imgSlider.style.transition = 'all 0.8s cubic-bezier(0.87, 0, 0.13, 1)'
+        textSlider.style.transition = 'all 0.8s cubic-bezier(0.87, 0, 0.13, 1)'
+        changeSlides()
 
-        lastTime = currentTime;
+        lastTime = currentTime
     }
 
     //reset the counter and transition and then change slides
     function counterIncrementHandler() {
         if (counter === 0) {
-            imgSlider.style.transition = "none";
-            textSlider.style.transition = "none";
-            counter = 2;
-            changeSlides();
-        } else if (counter === 3) {
-            imgSlider.style.transition = "none";
-            textSlider.style.transition = "none";
-            counter = 1;
-            changeSlides();
+            imgSlider.style.transition = 'none'
+            textSlider.style.transition = 'none'
+            counter = 2
+            changeSlides()
+        } else if (counter === images.length - 1) {
+            imgSlider.style.transition = 'none'
+            textSlider.style.transition = 'none'
+            counter = 1
+            changeSlides()
         }
     }
-})();
+
+    // duplicate node to put before and after
+    function cloneImage(elem, refElem, arr) {
+        const clone = elem.cloneNode(true)
+        arr.insertBefore(clone, refElem)
+        return clone
+    }
+})()
